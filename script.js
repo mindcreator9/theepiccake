ed63a21da00a235ded39bf1497fe5866067a487c
mindcreator9 <theepiccake.corp@gmail.com>
2024-08-09 14:27:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("accept-terms").addEventListener("click", function() {
+    document.getElementById("terms-modal").style.display = "none";
+    document.getElementById("main-content").classList.remove("hidden");
+});
